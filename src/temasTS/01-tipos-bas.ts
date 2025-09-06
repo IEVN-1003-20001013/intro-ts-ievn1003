b69db56392391ec48fbe8a2d10bc05e3be91b800
IEVN-1003-20001013 <carpiolegacy@gmail.com>
2025-09-05 18:45:57 -0600

    segundo commit 05-09-2025

---
+++ src/temasTS/01-tipos-bas.ts
@@ -0,0 +1,19 @@
+console.log("hola mundo")
+
+var num1= 23;
+let num2 = 32;
+const num3 = 84;
+
+let nombre:string
+let num:number
+let activo:boolean
+let sum = num1+num2+num3;
+
+console.log("la suma es:"+sum);
+
+let matricula:number|string
+
+matricula=12345678
+matricula='gahqujjqnajko'
+
+console.log(matricula)
